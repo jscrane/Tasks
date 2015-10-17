cdaec50d558030acbf2a40f5d3ee7df4b9238abc
Stephen Crane <jscrane@gmail.com>
2015-10-17 13:56:26 +0100

    randomised sleep

--- examples/atmega328p/Philosophers.ino
+++ examples/atmega328p/Philosophers.ino
@@ -8,19 +8,19 @@ static byte ids = 1;
 static byte leds[] = { 5, 6, 7, 8, 9, 10, 11, 12 };
 
 void eating(byte id, byte weight) {
-	Serial.print("Philosopher ");
+	Serial.print(F("Philosopher "));
 	Serial.print(id);
-	Serial.print(" eats: ");
+	Serial.print(F(" eats: "));
 	Serial.println(weight);
 
 	digitalWrite(leds[id], HIGH);
 
-	Tasks::delay(500);
+	Tasks::delay(1 + random(MAX * 100));
 
 	digitalWrite(leds[id], LOW);
 }
 
-class Philosopher: public Task<100> {
+class Philosopher: public Task<50> {
 public:
 	void setup() {
 		_id = ids++;
@@ -43,8 +43,11 @@ private:
 Philosopher philosophers[MAX-1];
 
 void setup() {
-	Serial.begin(9600);
+	Serial.begin(115200);
+	randomSeed(analogRead(0));
+
 	Tasks::init();
+	Tasks::set_idle_handler(timer_sleep);
 	for (int i = 0; i < MAX; i++) {
 		forks[i].signal();
 		pinMode(leds[i], OUTPUT);
