9dd6e995c40d12685740c12c83066fec65aeaf80
Stephen Crane <jscrane@gmail.com>
2017-10-05 07:22:36 +0100

    share init.cpp

--- TODO.md
+++ TODO.md
@@ -1,5 +1,4 @@
 To do:
 ======
 - per-thread sleeping (pseudo device for select time)
-- port to attiny84 (sharing init.cpp as attiny/init.cpp)
-- more examples...
+- fix examples for Arduino/Energia
--- src/proc_arch.h
+++ src/proc_arch.h
@@ -1,7 +1,9 @@
 #if defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
-#define ARCH atmega328p
+ || defined(__AVR_ATmega168__) || defined(__AVR_ATmega168P__)
+#define ARCH atmega
 #elif defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__)
-#define ARCH attiny85
+ ||   defined(__AVR_ATtiny84__) || defined(__AVR_ATtiny44__)
+#define ARCH attiny
 #elif defined(__AVR_ATtiny84__) || defined(__AVR_ATtiny44__)
 #define ARCH attiny84
 #elif defined(__MSP430FR5739__) || defined(__MSP430G2553__)
